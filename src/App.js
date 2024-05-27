4ebbd9fb18986fb09182c9921feae1346e9b5d21
Threshik <threshikavijay@gmail.com>
2024-05-28 00:20:27 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const name = <h1>Threshi</h1>;
+  // const age = <h2>20</h2>;
+  // const email = <h3>threshi@gmail.com</h3>;
+  // const user = (
+  //   <div>
+  //     {name}
+  //     {age}
+  //     {email}
+  //   </div>
+  //)
+
+  //name in single quotes and number in {}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <User name="andrea" age={20} email="andrea@gmail.com"/>
+    <User name="gill" age={23} email="gill@gmail.com"/>
+    <Job salary={20000} position="SDE" company="Amazon"/>
+    <Job salary={30000} position="SDE" company="Google"/>
+    </div>  //without initialling and declaring the variable and return the variable we can directly give the text between them.
   );
+}
+
+const User = (props) =>{
+  return (
+    <div>
+        <h1>{props.name}</h1>
+        <h2>{props.age}</h2>
+        <h3>{props.email}</h3>
+    </div>
+  )
+}
+
+const Job = (props) =>{
+  return(
+    <div>
+      <h1>{props.salary}</h1>
+      <h2>{props.position}</h2>
+      <h3>{props.company}</h3>
+    </div>
+  )
 }
 
 export default App;
